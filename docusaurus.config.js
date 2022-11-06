a69533f198e41cf047ae33d035230cf8fd89ffc1
Tao Wang <huwangtao123@gmail.com>
2022-11-05 17:26:37 -0700

    Add support for local search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,17 @@ const config = {
     }
   },
 
+  themes: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        language: ['en', 'zh']
+      }
+    ]
+  ],
+
   presets: [
     [
       'classic',
